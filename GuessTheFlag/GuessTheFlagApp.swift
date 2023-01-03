166244bbfa5c50d84f3880089d9e9dc817f685b5
yerdi <yerdi.isman@gmail.com>
2023-01-03 20:23:31 +0600

    first commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Yerdaulet Ismanaliyev on 03.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
